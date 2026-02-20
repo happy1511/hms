837aad90c7728724610b2750d5c2df76fb6dbb92
happy1511 <140176910+happy1511@users.noreply.github.com>
2026-02-20 22:57:19 +0530

    fix: db seeding issue

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,12 +5,14 @@ import {
 } from "@/generated/prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import data from "./india-locations.json";
+import "dotenv/config";
 
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  port: Number(process.env.DATABASE_PORT),
   connectionLimit: 5,
 });
 
